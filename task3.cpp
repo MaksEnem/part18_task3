1bb025adb2697dc0ce7878f57616d39a01e21081
MaksEnem <110655471+MaksEnem@users.noreply.github.com>
2023-05-04 16:35:44 +1100

    'srcond'

--- task3.cpp
+++ task3.cpp
@@ -1,36 +1,40 @@
 #include <iostream>
 
-using namespace std;
+void varik(int new_place, int var) {
 
-void bl(int c)
-{
-    for (int i = 0; i < c; ++i) cout << "  ";
+	int L = 7;
+	int R = 2;
+
+	if ((new_place < 0) || (new_place > L)) {
+
+		return;
+
+	}
+	if (new_place == L) {
+
+		var++;
+
+	}
+
+	for (int i = 1; i <= R; ++i) {
+
+		varik(new_place + i, var);
+
+	}
+	std::cout << var << " ";
 }
 
-int n = 6;
 
-int f(int x, int l)
-{
-    int r = 0;
 
-    for (int i = 1; i < 3; ++i)
-        if (x + i == n) {
-            bl(l); 
-            cout << i << " (ok)" << endl; 
-            return ++r; 
-        }
-        else if (x + i < n) {
-            bl(l); 
-            cout << i << ' ' << endl;
-            r += f(x + i, l + 1); }
-
-    return r;
-}
 
 int main()
 {
 
-    cout << f(3, 2) << endl;
 
-    return 0;
+	
+	int new_place = 0;
+	int var = 0;
+
+	varik(new_place, var);
+	
 }
